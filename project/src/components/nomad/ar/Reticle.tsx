a86aeed76e993febeff062e3394d61ccc5ef22e7
admin <admin@G304-CC0236.local>
2026-07-03 13:13:13 +0800

    as

--- project/src/components/nomad/ar/Reticle.tsx
+++ project/src/components/nomad/ar/Reticle.tsx
@@ -1,4 +1,3 @@
-/** Centre targeting reticle. Snaps + glows green when a marker locks on. */
 export function Reticle({ locked }: { locked: boolean }) {
   return (
     <div className="pointer-events-none absolute left-1/2 top-1/2 z-20 -translate-x-1/2 -translate-y-1/2">
@@ -14,12 +13,18 @@ export function Reticle({ locked }: { locked: boolean }) {
               : "border-white/40"
           }`}
         />
-        {/* four ticks */}
-        {["top-0 left-1/2 -translate-x-1/2 h-3 w-px", "bottom-0 left-1/2 -translate-x-1/2 h-3 w-px", "left-0 top-1/2 -translate-y-1/2 w-3 h-px", "right-0 top-1/2 -translate-y-1/2 w-3 h-px"].map(
-          (cls) => (
-            <span key={cls} className={`absolute ${cls} ${locked ? "bg-[#34e0a1]" : "bg-white/70"}`} />
-          ),
-        )}
+
+        {[
+          "top-0 left-1/2 -translate-x-1/2 h-3 w-px",
+          "bottom-0 left-1/2 -translate-x-1/2 h-3 w-px",
+          "left-0 top-1/2 -translate-y-1/2 w-3 h-px",
+          "right-0 top-1/2 -translate-y-1/2 w-3 h-px",
+        ].map((cls) => (
+          <span
+            key={cls}
+            className={`absolute ${cls} ${locked ? "bg-[#34e0a1]" : "bg-white/70"}`}
+          />
+        ))}
         <span
           className={`size-1.5 rounded-full transition-colors ${
             locked ? "bg-[#34e0a1]" : "bg-white/80"
